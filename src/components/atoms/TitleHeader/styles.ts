bd2bb8dfa4012f54a8b69453677671ab185fe6c6
Estevão Watanabe <estevao.watanabe@gmail.com>
2021-10-16 01:21:18 -0300

    Change some styles

---
+++ src/components/atoms/TitleHeader/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TitleHeader = styled.span`
+   color: ${props => props.theme.text.default};
+   font-size: 1.25rem;
+   font-weight: bold;
+`;
+
+export default TitleHeader;
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Login = styled.main`
+   background-color: ${props => props.theme.primary};
+`;
+
+export default Login;
